f1c3ebbcf9c7e05031be4c6d834f326c81e30bde
Nikki Petersen <piknik@me.com>
2020-07-24 16:32:05 -0600

    some basic layout

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # hero-profile
+A template for a website for artists and creatives.
 
-## Project setup
-```
-npm install
-```
+### Live Site
 
-### Compiles and hot-reloads for development
-```
-npm run serve
-```
-
-### Compiles and minifies for production
-```
-npm run build
-```
-
-### Lints and fixes files
-```
-npm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,96 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <section id="hero1" class="hero is-large image is-fullwidth" >
+      <div class="hero-body">
+        <div class="level">
+          <div class="level-item has-text-left">
+            <figure class="image is-fullwidth is-4x3 ">
+              <img class="pins" src="../public/bppinslogo.png" alt="baking power pins logo">
+            </figure>
+          </div>
+          <div class="level-item">
+            <img class="name" src="../public/bakingpowerlogo.png" alt="baking power logo">
+            <h1 class="has-text-right">I'm Nikki Petersen and I've got Baking Power!</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="hero2" class="hero is-info is-large" >
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title"> Hero #2</h1>
+        </div>
+      </div>
+    </section>
+
+    <section id="hero3" class="hero is-large" >
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title"> Hero #3</h1>
+        </div>
+      </div>
+    </section>
+
+    <section id="hero4" class="hero is-large" >
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title"> Hero #4</h1>
+        </div>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
 <style lang="scss">
 #app {
+
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #FFFFFF;
 }
+
+#hero1 {
+  background-image: url("../public/bread.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-size: cover;
+  height: 25%;
+}
+
+#hero2 {
+  background-size: cover;
+  height: 25%;
+}
+
+#hero3 {
+  background-size: cover;
+  height: 25%;
+}
+
+#hero4 {
+  background-image: url("../public/candy.jpg");
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  height: 25%;
+}
+
+  .pins {
+    width: 150px;
+  }
+
+  .name {
+    width: 350px;
+  }
 </style>
